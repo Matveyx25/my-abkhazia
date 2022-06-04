b2621d14e8010642ea030e2d2b7874a3a5644eb3
Matveyx25 <matveyx25@gmail.com>
2022-06-04 18:06:32 +0400

    Registration

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,13 +2,25 @@ import React, { useState } from "react";
 import { Modal } from "../Modal/Modal";
 import s from "./Login.module.css";
 import Checkbox from '../Checkbox/Checkbox';
+import Radio from "../Radio/Radio";
 
 export const Login = ({ visible, onClose }) => {
   const [typeModal, setTypeModal] = useState("login");
 
   const [name, setName] = useState("");
+  const [LastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [EmailReg, setEmailReg] = useState("");
   const [visiblePassword, changeVisiblePassword] = useState(false);
+  const [visiblePasswordReg, changeVisiblePasswordReg] = useState(false);
+  const [visiblePassword2Reg, changeVisiblePassword2Reg] = useState(false);
   const [password, setPassword] = useState("");
+  const [country, setCountry] = useState("");
+  const [phone, setPhone] = useState("");
+  const [passwordReg, setPasswordReg] = useState("");
+  const [password2Reg, setPassword2Reg] = useState("");
+
+  const [accType, setAccType] = useState("");
 
   return (
     <Modal visible={visible} onClose={onClose}>
@@ -41,8 +53,8 @@ export const Login = ({ visible, onClose }) => {
               <input
                 placeholder="Введите адрес электронной почты"
                 type="text"
-                value={name}
-                onChange={(event) => setName(event.target.value)}
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
                 required
               />
             </div>
@@ -84,6 +96,165 @@ export const Login = ({ visible, onClose }) => {
                 <img src="../images/gridicons_phone.svg"/>
                 С помощью номера телефона
             </a>
+            <a href="#" className={s.withPhone}>
+                <img src="../images/google-fill.svg"/>
+                С помощью Google
+            </a>
+          </form>
+        )}
+
+        {typeModal == "reg" && (
+          <form className={s.form}>
+            <div className={s.inputBlock}>
+            <p>Тип пользователя*</p>
+            <input
+              placeholder="Выберите тип пользователя"
+              type="text"
+              value={accType}
+              onChange={(event) => setAccType(accType)}
+              className="account-page__input-with-radio"
+              required
+            />
+             <div className="account-page__radios">
+              <Radio
+                labelText="Турист"
+                inputId="turist"
+                inputName="accType"
+                inputValue="Турист"
+                changeFunction={(event) => setAccType(event.target.value)}
+              />
+              <Radio
+                labelText="Поставщик услуг"
+                inputId="postav"
+                inputName="accType"
+                inputValue="Поставщик услуг"
+                changeFunction={(event) => setAccType(event.target.value)}
+              />
+              <Radio
+                labelText="Партнер"
+                inputId="partner"
+                inputName="accType"
+                inputValue="Партнер"
+                changeFunction={(event) => setAccType(event.target.value)}
+              />
+            </div>
+          </div>
+            <div className={s.inputRow}>
+                <div className={s.inputBlock}>
+                <p>Имя*</p>
+                <input
+                    placeholder="Введите Ваше имя"
+                    type="text"
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                    required
+                />
+                </div>
+                <div className={s.inputBlock}>
+                <p>Фамилия*</p>
+                <input
+                    placeholder="Введите Вашу фамилию"
+                    type="text"
+                    value={LastName}
+                    onChange={(event) => setLastName(event.target.value)}
+                    required
+                />
+                </div>
+            </div>
+            <div className={s.inputBlock}>
+              <p>E-mail*</p>
+              <input
+                placeholder="Введите адрес электронной почты"
+                type="text"
+                value={EmailReg}
+                onChange={(event) => setEmailReg(event.target.value)}
+                required
+              />
+            </div>
+            <div className={s.inputRow}>
+                <div className={s.inputBlock}>
+                <p>Страна*</p>
+                <input
+                    placeholder="Россия"
+                    type="text"
+                    value={country}
+                    onChange={(event) => setCountry(event.target.value)}
+                    required
+                />
+                </div>
+                <div className={s.inputBlock}>
+                <p>Номер телефона*</p>
+                <input
+                    placeholder="+7 (000) 000-00-00"
+                    type="text"
+                    value={phone}
+                    onChange={(event) => setPhone(event.target.value)}
+                    required
+                />
+                </div>
+            </div>
+            <div className={s.inputRow}>
+                <div className={s.inputBlock}>
+                    <p>Пароль*</p>
+                    <div className="account-page__input-position">
+                        <input
+                        placeholder="Пароль"
+                        value={passwordReg}
+                        onChange={(event) => setPasswordReg(event.target.value)}
+                        type={visiblePasswordReg ? "text" : "password"}
+                        required
+                        />
+                        {visiblePasswordReg ? (
+                        <img
+                            src="../images/eye-invisible-filled.svg"
+                            className={s.eye}
+                            onClick={() => changeVisiblePasswordReg(!visiblePasswordReg)}
+                        />
+                        ) : (
+                        <img
+                            src="../images/eye-filled.svg"
+                            className={s.eye}
+                            onClick={() => changeVisiblePasswordReg(!visiblePasswordReg)}
+                        />
+                        )}
+                    </div>
+                </div>
+                <div className={s.inputBlock}>
+                    <p>Повторите пароль*</p>
+                    <div className="account-page__input-position">
+                        <input
+                        placeholder="Пароль"
+                        value={password2Reg}
+                        onChange={(event) => setPassword2Reg(event.target.value)}
+                        type={visiblePassword2Reg ? "text" : "password"}
+                        required
+                        />
+                        {visiblePassword2Reg ? (
+                        <img
+                            src="../images/eye-invisible-filled.svg"
+                            className={s.eye}
+                            onClick={() => changeVisiblePassword2Reg(!visiblePassword2Reg)}
+                        />
+                        ) : (
+                        <img
+                            src="../images/eye-filled.svg"
+                            className={s.eye}
+                            onClick={() => changeVisiblePassword2Reg(!visiblePassword2Reg)}
+                        />
+                        )}
+                    </div>
+                </div>
+            </div>
+            <div className={s.forget}>
+                <Checkbox labelText="Нажимая на кнопку, я соглашаюсь с Условиями и Политикой конфиденциальности" inputId="forget"/>
+            </div>
+            <button className={s.submitBtn}>Войти</button>
+            <p className={s.withStar}>* Поля обязательны для заполнения</p>
+            <div className={s.or}>
+                <span></span>
+                <p>Или</p>
+                <span></span>
+            </div>
             <a href="#" className={s.withPhone}>
                 <img src="../images/google-fill.svg"/>
                 С помощью Google
